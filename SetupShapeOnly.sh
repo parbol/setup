24e8784a26136b64cd418fa9e9ef005c54f9b054
Piergiulio <piergiulio.lenzi@cern.ch>
2018-03-16 11:01:38 +0100

    seup for nanoAod

--- SetupShapeOnly.sh
+++ SetupShapeOnly.sh
@@ -82,6 +82,22 @@ elif [[ "$CMSSW_VERSION" == CMSSW_8_*_* ]]; then
     echo " ...done."
     source ZZMatrixElement/setup.sh -j 12
 
+elif [[ "$CMSSW_VERSION" == CMSSW_9_*_* ]]; then
+    echo "======================================="
+    echo "running with $CMSSW_VERSION - this is a 13 TeV setup!"
+    echo "Current time:" $(date)
+    echo "checking out additional repositories; this could take a while ..."
+    echo "======================================="
+
+    echo " - Basic Code"
+
+    github-addext latinos/LatinoAnalysis.git LatinoAnalysis
+
+    echo " - Nano Tools"
+
+    git clone git@github.com:cms-nanoAOD/nanoAOD-tools.git PhysicsTools/NanoAODTools 
+
+
 else
     echo "======================================="
     echo "You are using release $CMSSW_VERSION which is not supported by this script."
