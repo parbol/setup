fa37c291c48fed9730a95200d277a0453888511a
Yutaro <yutaro.iiyama@cern.ch>
2018-12-13 19:53:36 +0100

    update multidraw tag to 2.0.3

--- SetupShapeOnly.sh
+++ SetupShapeOnly.sh
@@ -86,7 +86,7 @@ elif [[ "$CMSSW_VERSION" == CMSSW_8_*_* ]]; then
 
     git clone git@github.com:yiiyama/multidraw.git LatinoAnalysis/MultiDraw
     cd LatinoAnalysis/MultiDraw
-    git checkout 2.0.2 2>/dev/null
+    git checkout 2.0.3 2>/dev/null
     ./mkLinkDef.py --cmssw
 
 elif [[ "$CMSSW_VERSION" == CMSSW_9_*_* ]]; then
@@ -108,7 +108,7 @@ elif [[ "$CMSSW_VERSION" == CMSSW_9_*_* ]]; then
 
     git clone git@github.com:yiiyama/multidraw.git LatinoAnalysis/MultiDraw
     cd LatinoAnalysis/MultiDraw
-    git checkout 2.0.2 2>/dev/null
+    git checkout 2.0.3 2>/dev/null
     ./mkLinkDef.py --cmssw
 
 elif [[ "$CMSSW_VERSION" == CMSSW_10_*_* ]]; then
@@ -130,7 +130,7 @@ elif [[ "$CMSSW_VERSION" == CMSSW_10_*_* ]]; then
 
     git clone git@github.com:yiiyama/multidraw.git LatinoAnalysis/MultiDraw
     cd LatinoAnalysis/MultiDraw
-    git checkout 2.0.2 2>/dev/null
+    git checkout 2.0.3 2>/dev/null
     ./mkLinkDef.py --cmssw
 
 else
